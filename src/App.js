9c704d37f9fe9b9809caca45b66c71d0491ce2c4
unknown <martin_suarez_mendez@outlook.com>
2021-03-11 11:50:59 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React please and go to America
         </a>
       </header>
     </div>
